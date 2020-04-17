f8ea20eae2978d19c54befc846fd689e9c7cbe2b
nsoto92 <nor.soto92@gmail.com>
2020-04-16 22:23:24 -0700

    calculator

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,2 +1,21 @@
-const readline = require("readline-sync")
+var readlineSync = require("readline-sync")
 
+var num1 = readlineSync.questionInt("Enter your first number: ")
+var num2 = readlineSync.questionInt("Enter your second number: ")
+var operation = readlineSync.question("Please enter the operation to perform: ADD, SUBSTRACT, DIVIDE, MULTIPLY: ")
+
+if (operation === "ADD") {
+    return console.log("The result is " + Number(num1+num2))
+}
+
+if (operation === "SUBSTRACT") {
+    return console.log("The result is " + Number(num1-num2))
+}
+
+if (operation === "DIVIDE") {
+    return console.log("The result is " + Number(num1/num2))
+}
+
+if (operation === "MULTIPLY") {
+    return console.log("The result is " + Number(num1*num2))
+}
