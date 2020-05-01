5ebf6f5ded75e97b96f23b64f44de5787dfb1a27
nsoto92 <nor.soto92@gmail.com>
2020-04-30 17:10:02 -0700

    updates

--- forception/main.js
+++ forception/main.js
@@ -2,11 +2,35 @@ var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
 var alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 var newArr = []
+var caps = alphabet.toUpperCase()
+var letters = caps.split("")
 
-function forception(people, alphabet) {
-    for (i = 0; i < people.length; i++) {
-        newArr.push(people[i])
+
+
+for (var i = 0; i < people.length; i++) {
+
+    var result = newArr.push(people[i] + ": ".concat(letters))
+    for (var j = 0; j < letters.length; j++) {
     }
 }
+
 console.log(newArr)
+
+
+// function forception(a, b) {
+//     var newArr = []
+//     var caps = a.toUpperCase()
+//     var letters = caps.split("")
+
+//     for (var i = 0; i < b.length; i++) {
+
+//         var result = newArr.push(b[i] + ": ".concat(letters))
+//         for (var j = 0; j < letters.length; j++) {
+//         }
+//     }
+
+//     return newArr
+// }
+
+// forception(people, alphabet)
 
